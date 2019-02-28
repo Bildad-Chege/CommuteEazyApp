2d2993a3925e813f1635a3e44992fd9c6e2416c4
Bildad-Chege <43196637+Bildad-Chege@users.noreply.github.com>
2019-02-28 11:38:14 +0300

    update

--- app/src/main/java/com/example/commuteeazy/activities/HomePage.java
+++ app/src/main/java/com/example/commuteeazy/activities/HomePage.java
@@ -2,14 +2,31 @@ package com.example.commuteeazy.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.example.commuteeazy.R;
+import com.example.commuteeazy.adapters.OpListAdapter;
+
+import java.util.List;
 
 public class HomePage extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    List<String> opnames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        recyclerView = (RecyclerView) findViewById(R.id.operators_list);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        recyclerView.setHasFixedSize(true);
+        adapter = new OpListAdapter(this,opnames);
     }
 }
